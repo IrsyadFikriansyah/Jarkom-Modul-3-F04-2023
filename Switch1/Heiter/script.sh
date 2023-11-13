40ac1b0f7df7359e5ba6aca4ea335323fc8afafa
IrsyadFikriansyah <irsyadfikriansyah@gmail.com>
2023-11-13 23:19:24 +0700

    feat: add dns setting

--- Switch1/Heiter/script.sh
+++ Switch1/Heiter/script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo 'nameserver 192.168.122.1  # IP Pandudewanata' > /etc/resolv.conf
+echo 'nameserver 192.168.122.1  # IP Aura' > /etc/resolv.conf
 
 bash /root/install-script.sh
 
@@ -8,9 +8,13 @@ bash /root/install-script.sh
 cp /root/named.conf.local /etc/bind/named.conf.local
 cp /root/named.conf.options /etc/bind/named.conf.options
 
-# Setting baratayuda
-mkdir -p /etc/bind/baratayuda
-cp /root/baratayuda/baratayuda.abimanyu.f04.com /etc/bind/baratayuda/baratayuda.abimanyu.f04.com
+# Setting granz
+mkdir -p /etc/bind/granz
+cp /root/granz/granz.channel.f04.com /etc/bind/granz/granz.channel.f04.com
+
+# Setting riegel
+mkdir -p /etc/bind/riegel
+cp /root/riegel/riegel.canyon.f04.com /etc/bind/riegel/riegel.canyon.f04.com
 
 # Start services
 service bind9 restart
