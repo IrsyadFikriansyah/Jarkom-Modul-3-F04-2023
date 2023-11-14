d79f9805d07c4eaf4bc320b55e5dbee46f3a384f
IrsyadFikriansyah <irsyadfikriansyah@gmail.com>
2023-11-14 11:01:07 +0700

    feat remove unnacessary nameserver on clients

--- Switch4/Client/Sein/script.sh
+++ Switch4/Client/Sein/script.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-echo 'nameserver 192.223.1.3  # IP Heiter' > /etc/resolv.conf
-
 bash /root/install_script.sh
--- Switch4/Client/Stark/script.sh
+++ Switch4/Client/Stark/script.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-echo 'nameserver 192.223.1.3  # IP Heiter' > /etc/resolv.conf
 
 bash /root/install_script.sh
