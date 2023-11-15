18d252a8dcc102fb639d937833329d94c78487c2
gmaaliki <maalikighifari@gmail.com>
2023-11-15 15:00:53 +0700

    Update: Adding htop, less, and apache2-utils install

--- Switch3/Client/Revolte/install_script.sh
+++ Switch3/Client/Revolte/install_script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-  
+
 apt-get update > /dev/null
 
 # Check if dnsutils is installed
@@ -16,4 +16,28 @@ if ! dpkg -l | grep -q lynx; then
   apt-get install lynx -y
 else
   echo "lynx is already installed."
+fi
+
+# Check if apache2-utils is installed
+if ! dpkg -l | grep -q apache2-utils; then
+  echo "apache2-utils is not installed. Installing it now..."
+  apt-get install apache2-utils -y
+else
+  echo "apache2-utils is already installed"
+fi
+
+# Check if htop is installed
+if ! dpkg -l | grep -q htop; then
+  echo "htop is not installed. Installing it now..."
+  apt-get install htop -y
+else
+  echo "htop is already installed"
+fi
+
+# Check if less is installed
+if ! dpkg -l | grep -q less; then
+  echo "less is not installed. Installing it now..."
+  apt-get install less -y
+else
+  echo "less is already installed"
 fi
