9ee869af828e36f868c7eb3484eca053bb8b5453
gmaaliki <maalikighifari@gmail.com>
2023-11-16 10:03:54 +0700

    add: db server script

---
+++ Switch2/Denken/install_script.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+  
+apt-get update > /dev/null
+
+packages=("lsb-release" "ca-certificates" "apt-transport-https" "software-properties-common" "gnupg2" "mariadb-server" "htop" "wget")
+
+for package in "${packages[@]}"; do
+  # Check if the package is installed
+  if ! dpkg -l | grep -q "$package"; then
+    echo "$package is not installed. Installing it now..."
+    apt-get install "$package" -y
+  else
+    echo "$package is already installed."
+  fi
+done
+
+# Adding repository to download php8.0
+echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/sury-php.list
+
+wget -qO - https://packages.sury.org/php/apt.gpg | sudo apt-key add -
+
+packages=("php8.0-mbstring" "php8.0-xml" "php8.0-cli" "php8.0-common" "php8.0-intl" "php8.0-opcache" "php8.0-readline" "php8.0-mysql" "php8.0-fpm" "php8.0-curl" "unzip")
+
+for package in "${packages[@]}"; do
+  # Check if the package is installed
+  if ! dpkg -l | grep -q "$package"; then
+    echo "$package is not installed. Installing it now..."
+    apt-get install "$package" -y
+  else
+    echo "$package is already installed."
+  fi
+done
---
+++ Switch2/Denken/script.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo 'nameserver 192.223.1.3 # IP Heiter' > /etc/resolv.conf
+
+bash /root/install_script.sh
+
+service mysql restart
