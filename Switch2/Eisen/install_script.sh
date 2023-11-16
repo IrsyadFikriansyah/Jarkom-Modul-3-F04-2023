d66fe085fd15feec0bc851f006aa56f927c05784
IrsyadFikriansyah <irsyadfikriansyah@gmail.com>
2023-11-16 10:05:46 +0700

    feat: add authentification configuration on lb

--- Switch2/Eisen/install_script.sh
+++ Switch2/Eisen/install_script.sh
@@ -9,3 +9,11 @@ if ! dpkg -l | grep -q nginx; then
 else
   echo "nginx is already installed."
 fi
+
+# Check if apache2-utils is installed
+if ! dpkg -l | grep -q apache2-utils; then
+  echo "apache2-utils is not installed. Installing it now..."
+  apt-get install apache2-utils -y
+else
+  echo "apache2-utils is already installed."
+fi
--- Switch2/Eisen/script.sh
+++ Switch2/Eisen/script.sh
@@ -10,4 +10,10 @@ rm -rf /etc/nginx/sites-enabled/default
 cp /root/lb-jarkom /etc/nginx/sites-available/
 ln -sf /etc/nginx/sites-available/lb-jarkom /etc/nginx/sites-enabled
 
+# configure autentification
+mkdir -p /etc/nginx/rahasisakita/
+
+cp /root/.htpasswd /etc/nginx/rahasisakita/
+chmod 644 /etc/nginx/rahasisakita/.htpasswd
+
 service nginx restart
