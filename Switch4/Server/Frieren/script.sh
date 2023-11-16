5e2ce99bea073725d69f91480b34315a5db6c03a
IrsyadFikriansyah <irsyadfikriansyah@gmail.com>
2023-11-16 11:57:24 +0700

    fix: change wrong domain naming in php working

--- Switch4/Server/Frieren/script.sh
+++ Switch4/Server/Frieren/script.sh
@@ -3,12 +3,12 @@
 bash /root/install_script.sh
 
 # Configure Web using nginx
-mkdir -p /var/www/riegel
-cp -r /root/modul-3/* /var/www/riegel/
+mkdir -p /var/www/granz
+cp -r /root/modul-3/* /var/www/granz/
 
 rm -rf /etc/nginx/sites-enabled/default
-cp /root/riegel.canyon.f04 /etc/nginx/sites-available/riegel.canyon.f04
-ln -sf /etc/nginx/sites-available/riegel.canyon.f04 /etc/nginx/sites-enabled
+cp /root/granz.channel.f04 /etc/nginx/sites-available/granz.channel.f04
+ln -sf /etc/nginx/sites-available/granz.channel.f04 /etc/nginx/sites-enabled
 
 service nginx restart
 service php7.3-fpm start
